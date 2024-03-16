3ec4da0ca80b80010c3f79a53c1a9596d182c4f6
wavejumper3 <calculatorcoder42@gmail.com>
2024-03-15 20:21:38 -0400

    added a blog section!

---
+++ blogging/currentArticles.js
@@ -0,0 +1,3 @@
+let currentArticles = [{"name" : "A first post on my brand new blog!", "link": "/blogging/first-post.html", "year": 2024, "month": 3, "day" : 15},
+{"name" : "My pooch is my #1 pal", "link": "https://www.youtube.com/embed/cPgno6QqWQs?autoplay=1&mute=1", "year": 2024, "month": 3, "day" : 15}
+]
