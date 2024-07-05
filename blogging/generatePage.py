e128dd75b0574f3cb98b4f59538f057531f50e82
Wavejumper3 <calculatorcoder42@gmail.com>
2024-07-05 09:48:11 -0400

    page generation script style improved, fixed title functionality

--- blogging/generatePage.py
+++ blogging/generatePage.py
@@ -3,39 +3,36 @@ import json
 
 def main():
 	f = open("blog-boilerplate.html", "r")
-	
 	page = f.read()
-	
 	f.close()
+
 	f = open("currentArticles.js", "r")
-	pageList = f.read()
-	
+	page_list = f.read()
 	f.close()
 	
 	print("\n\n\n")
-	name = "/blogging/" + input("What's the blog link to use for the page?\n>") + ".html"
-	
-	if pageList.find(name) != -1:
+	name = input("What's the blog link to use for the page?\n>")
+
+	if page_list.find("/blogging/" + name + ".html") != -1:
 		print(i["name"] + " has the same link! uh ohhhhh")
 		exit()
-	
+	page = page.replace("###", name)
 	tag = ""
 	article = ""
-	while tag != "q":
-		tag = input("\nWhat tag? ('h1', 'h2', 'h3', 'p', 'br', 'img')")
-		if tag == "img":
-			article = article + "<img src='" + input("\n\nimg source? ") + "' alt=''" + input("alt? ") + "'>\n"
-		elif tag == "br":
-			article = article + "<br>"
-		else:
-			article = article + "<" + tag + ">" + input("\nenter text\n\n>") + "</" + tag + ">\n"
-		print("\nq to quit...")
-		tag = input(">")
+	while input("q to quit:") != "q":
+		tag = input("\nWhat tag? ('h1', 'h2', 'h3', 'p', 'br', 'img') \n")
+		article = add_tag(article, tag)
 	page = page.replace("$$$$$$$$", article)
-	f = open(name.lstrip("/blogging/"), "w")
+	f = open(name, "w")
 	f.write(page)
 	f.close()
 
+def add_tag(article, tag):
+	if tag == "img":
+		return article + "<img src='" + input("\nImage Location: ") + "'>"
+	if tag == "br":
+		return article + "<br>"
+	article = article + "<" + tag + ">" + input("\nText: ") + "</" + tag + ">"
 
 if __name__ == "__main__":
 	main()
