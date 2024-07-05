f741dc464953ef2c0b4f9bc86d3ae5640334542d
Wavejumper3 <calculatorcoder42@gmail.com>
2024-07-05 10:29:01 -0400

    added a page to my blog

--- blogging/currentArticles.js
+++ blogging/currentArticles.js
@@ -1,4 +1,5 @@
 let currentArticles = [{"name" : "A first post on my brand new blog!", "link": "/blogging/first-post.html", "year": 2024, "month": 3, "day" : 15},
 {"name" : "My pooch is my #1 pal", "link": "https://www.youtube.com/embed/cPgno6QqWQs?autoplay=1&mute=1", "year": 2024, "month": 3, "day" : 15},
-{"name" : "Made a page creation tool!", "link": "/blogging/autogen-page.html", "year": 2024, "month": 3, "day": 19}
+{"name" : "Made a page creation tool!", "link": "/blogging/autogen-page.html", "year": 2024, "month": 3, "day": 19},
+{"name" : "My thoughts on Blogging as of late", "link" : "/blogging/thoughts-on-blogging.html", "year" :2024, "month": 7, "day" : 5}
 ]
